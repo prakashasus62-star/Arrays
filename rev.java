93b1ada094abc51471278fe553e557cc50bcddad
Prakash p <prakashasus62@gmail.com>
2026-07-03 22:07:45 +0530

    Reverse programm

--- rev.java
+++ rev.java
@@ -1,19 +1,11 @@
-import java.util.Scanner;
 public class rev {
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int arr[]=new int[n];
-        for(int i=0; i<n; i++){
-            arr[i]=sc.nextInt();
+        String str="Hello wolrld!";
+        String rev="";
+        for(int i=str.length()-1;i>=0; i--){
+            rev=rev+str.charAt(i);
         }
-        for(int i=arr.length-1; i>=0; i--){
-            System.out.print(arr[i]+" ");
-
-        }
+        System.out.println(rev);
 
     }
-
-
-    
 }
