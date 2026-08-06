6537a0043a4e83fd61d675128b3f219455ac0656
prakash-stack <prakashasus62@gmail.com>
2026-08-06 22:01:28 +0530

    Implement logic to find and print leaders in array

---
+++ lead.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] arr = {16,17,4,3,5,2};
+
+        int max = arr[arr.length-1];
+
+        System.out.print(max + " ");
+
+        for(int i=arr.length-2;i>=0;i--){
+
+            if(arr[i]>max){
+
+                max = arr[i];
+                System.out.print(max+" ");
+            }
+        }
+    }
+}
